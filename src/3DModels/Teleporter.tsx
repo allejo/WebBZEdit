d1bd35a45ef45df001558d444d10b5c69b01fecd
Vladimir Jimenez <allejo@me.com>
2021-06-12 20:21:50 -0700

    Fix UV Mapping for Teleporter model

--- src/3DModels/Teleporter.tsx
+++ src/3DModels/Teleporter.tsx
@@ -21,74 +21,79 @@ const Teleporter = ({ obstacle, isSelected, onClick }: Props) => {
   const [, bzwSizeY, bzwSizeZ] = size;
   const handleOnClick = () => onClick(obstacle);
 
-  const [borderTexture, linkTexture] = useLoader<Texture[]>(TextureLoader, [
+  const worldUnitsSizeY = bzwSizeY + border;
+
+  const [_borderTexture, _linkTexture] = useLoader<Texture[]>(TextureLoader, [
     teleporterBorder,
     teleporterLink,
   ]);
 
+  const borderTexture = _borderTexture.clone();
+  const linkTexture = _linkTexture.clone();
+
   borderTexture.wrapS = borderTexture.wrapT = RepeatWrapping;
   linkTexture.wrapS = linkTexture.wrapT = RepeatWrapping;
 
-  // @TODO: Fix texturing of the frame
   // @TODO: Add custom geometry of `isSelected`
+
+  // Link Texture
+
+  linkTexture.repeat.set(1, bzwSizeZ / (worldUnitsSizeY * 2) + border / 16);
+  linkTexture.needsUpdate = true;
+
+  //
+  // Top Frame Textures
+  //
+  const topFrameFrontTexture = borderTexture.clone();
+  topFrameFrontTexture.repeat.set(5, 0.5);
+  topFrameFrontTexture.needsUpdate = true;
+
+  const topFrameBackTexture = borderTexture.clone();
+  topFrameBackTexture.repeat.set(5, 0.5);
+  topFrameBackTexture.offset.set(0, 0.5);
+  topFrameBackTexture.needsUpdate = true;
+
+  const topFrameTopTexture = borderTexture.clone();
+  topFrameTopTexture.repeat.set(0, 5);
+  topFrameTopTexture.needsUpdate = true;
+
+  const topFrameLeftTexture = borderTexture.clone();
+  topFrameLeftTexture.repeat.set(0.5, 0.5);
+  topFrameLeftTexture.offset.set(0, 1);
+  topFrameLeftTexture.needsUpdate = true;
+
+  const topFrameRightTexture = borderTexture.clone();
+  topFrameRightTexture.repeat.set(0.5, 0.5);
+  topFrameRightTexture.offset.set(0.5, 1);
+  topFrameRightTexture.needsUpdate = true;
+
+  //
+  // Side Frame Textures
+  //
+  const sideFirstHalfTexture = borderTexture.clone();
+  sideFirstHalfTexture.repeat.set(0.5, 9);
+  sideFirstHalfTexture.needsUpdate = true;
+
+  const sideSecondHalfTexture = borderTexture.clone();
+  sideSecondHalfTexture.repeat.set(0.5, 9);
+  sideSecondHalfTexture.offset.set(-0.5, 0);
+  sideSecondHalfTexture.needsUpdate = true;
+
+  const sideBottomTexture = borderTexture.clone();
+  sideBottomTexture.repeat.set(1, 1);
+  sideBottomTexture.needsUpdate = true;
 
   return (
     <mesh
       position={[bzwPosX, bzwPosZ, bzwPosY]}
       rotation={[0, -deg2rad(rotation), 0]}
     >
-      {/* === Top Frame === */}
-      <SkinnableBox
-        position={[0, 0, bzwSizeZ]}
-        size={[border / 2, bzwSizeY + border * 2, border]}
-        rotation={0}
-        onClick={handleOnClick}
-        isSelected={false}
-        isSelectable={false}
-        topMaterial={borderTexture}
-        botMaterial={borderTexture}
-        xPosMaterial={borderTexture}
-        xNegMaterial={borderTexture}
-        yPosMaterial={borderTexture}
-        yNegMaterial={borderTexture}
-      />
-      {/* === Left Frame === */}
-      <SkinnableBox
-        position={[0, bzwSizeY + border * 1.5, 0]}
-        size={[border / 2, border / 2, bzwSizeZ]}
-        rotation={0}
-        onClick={handleOnClick}
-        isSelected={false}
-        isSelectable={false}
-        topMaterial={borderTexture}
-        botMaterial={borderTexture}
-        xPosMaterial={borderTexture}
-        xNegMaterial={borderTexture}
-        yPosMaterial={borderTexture}
-        yNegMaterial={borderTexture}
-      />
-      {/* === Right Frame === */}
-      <SkinnableBox
-        position={[0, -(bzwSizeY + border * 1.5), 0]}
-        size={[border / 2, border / 2, bzwSizeZ]}
-        rotation={0}
-        onClick={handleOnClick}
-        isSelected={false}
-        isSelectable={false}
-        topMaterial={borderTexture}
-        botMaterial={borderTexture}
-        xPosMaterial={borderTexture}
-        xNegMaterial={borderTexture}
-        yPosMaterial={borderTexture}
-        yNegMaterial={borderTexture}
-      />
       {/* === Link material === */}
       <SkinnableBox
         position={[0, 0, 0]}
-        size={[0.1, bzwSizeY + border, bzwSizeZ]}
+        size={[0.05, worldUnitsSizeY, bzwSizeZ]}
         rotation={0}
         onClick={handleOnClick}
-        isSelected={false}
         isSelectable={false}
         topMaterial={linkTexture}
         botMaterial={linkTexture}
@@ -97,6 +102,52 @@ const Teleporter = ({ obstacle, isSelected, onClick }: Props) => {
         yPosMaterial={linkTexture}
         yNegMaterial={linkTexture}
       />
+      {border > 0 && (
+        <>
+          {/* === Top Frame === */}
+          <SkinnableBox
+            position={[0, 0, bzwSizeZ]}
+            size={[border / 2, bzwSizeY + border * 2, border]}
+            rotation={0}
+            onClick={handleOnClick}
+            isSelectable={false}
+            topMaterial={topFrameTopTexture}
+            botMaterial={topFrameTopTexture}
+            xPosMaterial={topFrameRightTexture}
+            xNegMaterial={topFrameLeftTexture}
+            yPosMaterial={topFrameFrontTexture}
+            yNegMaterial={topFrameBackTexture}
+          />
+          {/* === Left Frame === */}
+          <SkinnableBox
+            position={[0, bzwSizeY + border * 1.5, 0]}
+            size={[border / 2, border / 2, bzwSizeZ]}
+            rotation={0}
+            onClick={handleOnClick}
+            isSelectable={false}
+            topMaterial={sideBottomTexture}
+            botMaterial={sideBottomTexture}
+            xPosMaterial={sideFirstHalfTexture}
+            xNegMaterial={sideFirstHalfTexture}
+            yPosMaterial={sideSecondHalfTexture}
+            yNegMaterial={sideSecondHalfTexture}
+          />
+          {/* === Right Frame === */}
+          <SkinnableBox
+            position={[0, -(bzwSizeY + border * 1.5), 0]}
+            size={[border / 2, border / 2, bzwSizeZ]}
+            rotation={0}
+            onClick={handleOnClick}
+            isSelectable={false}
+            topMaterial={sideBottomTexture}
+            botMaterial={sideBottomTexture}
+            xPosMaterial={sideSecondHalfTexture}
+            xNegMaterial={sideSecondHalfTexture}
+            yPosMaterial={sideFirstHalfTexture}
+            yNegMaterial={sideFirstHalfTexture}
+          />
+        </>
+      )}
     </mesh>
   );
 };
