d791735e83fa475bd461fc6e2f91bb3c88cc33be
Vladimir Jimenez <allejo@me.com>
2021-08-08 13:34:43 -0700

    Add highlightable bbox + effect to tank model

--- src/3DModels/Tank.tsx
+++ src/3DModels/Tank.tsx
@@ -1,5 +1,6 @@
 import { ThreeEvent, useLoader } from '@react-three/fiber';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { BoxGeometry, EdgesGeometry, LineSegments } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 import { ITankModel } from '../Document/Obstacles/TankModel';
@@ -11,13 +12,38 @@ interface Props {
   onClick: (configuration: ITankModel) => void;
 }
 
-const Tank = ({ configuration, onClick }: Props) => {
+const Tank = ({ configuration, isSelected, onClick }: Props) => {
   const { position, rotation = 0 } = configuration;
+  const [hover, setHover] = useState(false);
+  const segments = useRef<LineSegments>();
+  // Approximate bounding box for the tank model (with magic numbers)
+  const bbox = useRef(new EdgesGeometry(new BoxGeometry(8, 3, 2.25)));
   const gltf = useLoader(GLTFLoader, '/assets/tank.glb');
-  const handleOnClick = (event: ThreeEvent<MouseEvent>) => {
-    event.stopPropagation();
+
+  const isHighlighted = isSelected || hover;
+
+  const handleOnClick = (e: ThreeEvent<MouseEvent>) => {
+    e.stopPropagation();
     onClick(configuration);
   };
+  const handlePointerOver = (e: ThreeEvent<PointerEvent>) => {
+    e.stopPropagation();
+    setHover(true);
+  };
+  const handlePointerOut = (e: ThreeEvent<PointerEvent>) => {
+    e.stopPropagation();
+    setHover(false);
+  };
+
+  useEffect(() => {
+    gltf.scene.traverse(function (object: any) {
+      if (!object.isMesh) {
+        return;
+      }
+
+      object.material.color.set(isHighlighted ? 0x393939 : 0xffffff);
+    });
+  }, [gltf, isHighlighted]);
 
   return (
     <mesh
@@ -25,8 +51,23 @@ const Tank = ({ configuration, onClick }: Props) => {
       rotation={[deg2rad(270), 0, deg2rad(rotation)]}
       position={position}
       onClick={handleOnClick}
+      onPointerOver={handlePointerOver}
+      onPointerOut={handlePointerOut}
     >
       <primitive object={gltf.scene} />
+      {/* === BBox Highlight === */}
+      <lineSegments
+        ref={segments}
+        geometry={bbox.current}
+        // more magic numbers since our tank isn't perfectly centered
+        position={[0.9, 0, 1.125]}
+      >
+        <lineBasicMaterial
+          color={0x00ffff}
+          transparent={!isHighlighted}
+          opacity={isHighlighted ? 1 : 0}
+        />
+      </lineSegments>
     </mesh>
   );
 };
