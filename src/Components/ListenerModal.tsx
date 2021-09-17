e71cae7c51dbfa1ba9f443fc169d031fec75b31f
Vladimir Jimenez <allejo@me.com>
2021-09-17 00:28:29 -0700

    Allow ListenerModals to pass along event data

--- src/Components/ListenerModal.tsx
+++ src/Components/ListenerModal.tsx
@@ -1,29 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
 import { DialogStateReturn } from 'reakit';
 
 import eventBus from '../Utilities/EventBus';
 import Modal, { IModalProps } from './Modal';
 
-interface Props extends IModalProps {
+interface Props<T> extends Omit<IModalProps, 'children'> {
   dialog: DialogStateReturn;
   event: string;
   onOpen?: () => void;
+  children: ReactNode | ((eventData?: T) => ReactNode);
 }
 
 /**
  * A wrapper around the `Modal` component that will automatically show itself
- * when the specified event name is dispatched.
+ * when the specified event name is dispatched. When the child given for this
+ * Modal is a function, it will pass the event data as an argument that
+ * triggered this Modal to appear.
  *
  * @see Modal
  */
-const ListenerModal = ({ event, dialog, onOpen, ...props }: Props) => {
+const ListenerModal = <T,>({ event, dialog, onOpen, ...props }: Props<T>) => {
+  const [eventData, setEventData] = useState<T>();
   const eventBusCallbackId = useRef('');
 
   // Register this component as a modal that listens to an event
   useEffect(() => {
-    eventBusCallbackId.current = eventBus.on(event, () => {
+    eventBusCallbackId.current = eventBus.on(event, (data: T) => {
       dialog.show();
       onOpen?.();
+      setEventData(data);
     });
 
     return () => {
@@ -33,7 +38,9 @@ const ListenerModal = ({ event, dialog, onOpen, ...props }: Props) => {
 
   return (
     <Modal dialog={dialog} {...props}>
-      {props.children}
+      {typeof props.children === 'function'
+        ? props.children(eventData)
+        : props.children}
     </Modal>
   );
 };
