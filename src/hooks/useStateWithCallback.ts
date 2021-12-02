94281bd1701f0ebfce86a3feb3c825cbd3e9496d
Vladimir Jimenez <allejo@me.com>
2021-12-02 09:53:34 -0800

    Improve useStateWithCallback hook
    - Wrap state value inside of an object to force state updates to trigger
      effects
    - Support setter callbacks for setState functions

--- src/hooks/useStateWithCallback.ts
+++ src/hooks/useStateWithCallback.ts
@@ -1,7 +1,24 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import {
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
-type Callback<T> = ((newState: T) => void) | null;
-type ReturnType<T> = [T, (newState: T, callback?: Callback<T>) => void];
+/**
+ * We store state inside of an object to force React to trigger a state update
+ * since hooks will not trigger an update if the "new" state value is the same
+ * as the previous state value.
+ *
+ * @link https://reactjs.org/docs/hooks-reference.html#bailing-out-of-a-state-update
+ */
+type State<T> = { value: T };
+type Callback<T> = (newState: T) => void;
+type ReturnType<T> = [
+  T,
+  (newState: SetStateAction<T>, callback?: Callback<T>) => void,
+];
 
 /**
  * Similar to `useState`, except it has support for triggering a callback after
@@ -10,16 +27,21 @@ type ReturnType<T> = [T, (newState: T, callback?: Callback<T>) => void];
  * @param initialState The initial value that will be stored
  *
  * @link https://git.io/JM61A
+ * @link https://stackoverflow.com/a/61842546
  */
 export function useStateWithCallback<T>(initialState: T): ReturnType<T> {
-  const [state, setState] = useState<T>(initialState);
-  const [counter, setCounter] = useState(0); // @TODO nasty workaround
-  const callbacksRef = useRef<Callback<T>>();
+  const [state, setState] = useState<State<T>>({ value: initialState });
+  const callbacksRef = useRef<Callback<T> | null>();
 
+  // Use `useCallback` so that the `setState` function keeps the same ref and
+  // does not trigger subsequent re-renders.
   const setStateCallback = useCallback(
-    (newState: T, callback?: Callback<T>) => {
-      setState(newState);
-      setCounter((c) => c + 1);
+    (newState: SetStateAction<T>, callback?: Callback<T>) => {
+      setState(
+        (ps: State<T>): State<T> => ({
+          value: newState instanceof Function ? newState(ps.value) : newState,
+        }),
+      );
 
       if (callback) {
         callbacksRef.current = callback;
@@ -30,10 +52,10 @@ export function useStateWithCallback<T>(initialState: T): ReturnType<T> {
 
   useEffect(() => {
     if (callbacksRef.current) {
-      callbacksRef.current(state);
+      callbacksRef.current(state.value);
       callbacksRef.current = null;
     }
-  }, [counter, state]);
+  }, [state]);
 
-  return [state, setStateCallback];
+  return [state.value, setStateCallback];
 }
