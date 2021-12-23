2732582b787e1eb470aa53590cbdacad0c3a1f76
Vladimir Jimenez <allejo@me.com>
2021-12-22 16:49:30 -0800

    Add UI unit tests for TextField

---
+++ src/Components/Form/__tests__/TextField.tsx
@@ -0,0 +1,48 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import TextField from '../TextField';
+
+describe('TextField Component', () => {
+  it('should trigger `onChange` callback when user types', () => {
+    let currentValue = '';
+
+    const { getByLabelText } = render(
+      <TextField
+        label="Your Name"
+        onChange={(data) => (currentValue = data)}
+        value={currentValue}
+      />,
+    );
+    const field = getByLabelText('Your Name') as HTMLInputElement;
+
+    expect(field).toHaveValue('');
+    expect(currentValue).toEqual('');
+
+    const content = 'I am not log4j';
+    userEvent.type(field, content);
+    expect(currentValue).toEqual(content);
+  });
+
+  it('should not let further changes when `allowChange` returns false', () => {
+    let currentValue = '';
+
+    const { getByLabelText } = render(
+      <TextField
+        label="Your Name"
+        onChange={(data) => (currentValue = data)}
+        allowChange={(data) => data.length <= 5}
+        value=""
+      />,
+    );
+    const field = getByLabelText('Your Name') as HTMLInputElement;
+
+    expect(field).toHaveValue('');
+    expect(currentValue).toEqual('');
+
+    const content = 'I heart BZFlag';
+    userEvent.type(field, content);
+    expect(currentValue).toEqual(content.substr(0, 5));
+  });
+});
